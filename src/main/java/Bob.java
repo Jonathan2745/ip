48f39ebba4ed0323f920e666bd88c26146b67e83
Jonathan2745 <jonathan0934512@gmail.com>
2025-02-06 08:18:22 +0800

    Level-0

---
+++ src/main/java/Bob.java
@@ -0,0 +1,12 @@
+public class Bob {
+    public static void main(String[] args) {
+        // Prints Initial Greeting
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Bob");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
