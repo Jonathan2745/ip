ab993ab7659b0ba1c325e017144ef9a301dcdf1c
Jonathan2745 <jonathan0934512@gmail.com>
2025-02-06 08:20:52 +0800

    Level-1

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
         // Prints Initial Greeting
@@ -5,6 +7,21 @@ public class Bob {
         System.out.println(" Hello! I'm Bob");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        // Scans for input and repeats it back
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+
+
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
 
