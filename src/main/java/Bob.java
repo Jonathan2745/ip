f7d1a7662497521916b982f5e874dbaadbcd1b8b
Jonathan2745 <jonathan0934512@gmail.com>
2025-02-06 09:07:01 +0800

    Level 2

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -2,28 +2,59 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
+
+
         // Prints Initial Greeting
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Bob");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Scans for input and repeats it back
+        // Declare variables
         Scanner scanner = new Scanner(System.in);
         String input;
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
+
+        // Scans for variables
         while (true) {
             input = scanner.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 break;
+
             }
+
+        // "List" command - prints out list
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                if (taskCount == 0) {
+                    System.out.println(" No tasks added yet.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
+
+
+            } else {
+                if (taskCount < 100) {
+                    tasks[taskCount++] = input;
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" added: " + input);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Task list is full!");
+                    System.out.println("____________________________________________________________");
+                }
+            }
+
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+            System.out.println(" Bye. Hope to see you again soon!");
             System.out.println("____________________________________________________________");
+
         }
-
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-
     }
 }
