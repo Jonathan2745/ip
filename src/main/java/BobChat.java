b55d25cccf7416b7a382e6232315db0c3535d8fd
Jonathan2745 <e1157198@u.nus.edu>
2025-02-21 21:14:14 +0800

    added ArrayList<Task> and Level-5

--- src/main/java/BobChat.java
+++ src/main/java/BobChat.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import Errors.*;
 
 class Bob {
     private final Scanner scanner = new Scanner(System.in);
-    private final Task[] tasks = new Task[100];
-    private int taskCount = 0;
+    private final ArrayList<Task> tasks = new ArrayList<>();
 
     public void start() throws InputExceptions {
         System.out.println("____________________________________________________________");
@@ -32,11 +32,11 @@ class Bob {
                     case "list":
                         System.out.println("____________________________________________________________");
                         System.out.println(" Here are the tasks in your list:");
-                        if (taskCount == 0) {
+                        if (tasks.isEmpty()) {
                             System.out.println("No tasks added yet.");
                         } else {
-                            for (int i = 0; i < taskCount; i++) {
-                                System.out.println((i + 1) + ". " + tasks[i].toString());
+                            for (int i = 0; i < tasks.size(); i++) {
+                                System.out.println((i + 1) + ". " + tasks.get(i).toString());
                             }
                         }
                         System.out.println("____________________________________________________________");
@@ -45,23 +45,23 @@ class Bob {
                     case "mark":
                         if (arguments.isEmpty()) throw new InputExceptions.MissingArgumentException("mark");
                         int markIndex = Integer.parseInt(arguments) - 1;
-                        if (markIndex < 0 || markIndex >= taskCount) throw new InputExceptions("mark target must exist");
-                        tasks[markIndex].markAsDone();
+                        if (markIndex < 0 || markIndex > tasks.size()) throw new InputExceptions("mark target must exist");
+                        tasks.get(markIndex).markAsDone();
                         System.out.println("____________________________________________________________");
                         System.out.println(" Nice! I've marked this task as done:");
-                        System.out.println("   " + tasks[markIndex].toString());
+                        System.out.println("   " + tasks.get(markIndex).toString());
                         System.out.println("____________________________________________________________");
                         break;
 
                     case "unmark":
                         if (arguments.isEmpty()) throw new InputExceptions("unmark failed, no arguments");
                         int unmarkIndex = Integer.parseInt(arguments) - 1;
-                        if (unmarkIndex < 0 || unmarkIndex >= taskCount)
+                        if (unmarkIndex < 0 || unmarkIndex > tasks.size())
                             throw new InputExceptions.InvalidIndexException();
-                        tasks[unmarkIndex].markAsNotDone();
+                        tasks.get(unmarkIndex).markAsNotDone();
                         System.out.println("____________________________________________________________");
                         System.out.println(" OK, I've marked this task as not done yet:");
-                        System.out.println("   " + tasks[unmarkIndex].toString());
+                        System.out.println("   " + tasks.get(unmarkIndex).toString());
                         System.out.println("____________________________________________________________");
                         break;
 
@@ -69,16 +69,16 @@ class Bob {
                         if (arguments.isEmpty()) {
                             throw new InputExceptions.MissingArgumentException("todo");
                         }
-                        tasks[taskCount++] = new ToDo(arguments);
-                        System.out.println("Now you have " + taskCount + " tasks in the list.");
+                        tasks.add( new ToDo(arguments) );
+                        System.out.println("Now you have " + (tasks.size() + 1) + " tasks in the list.");
                         System.out.println("____________________________________________________________");
                         break;
 
                     case "deadline":
                         String[] parts = arguments.split(" /by ", 2);
                         if (parts.length < 2) throw new InputExceptions.MissingArgumentException("deadline");
-                        tasks[taskCount++] = new Deadline(parts[0], parts[1]);
-                        System.out.println("Now you have " + taskCount + " tasks in the list.");
+                        tasks.add( new Deadline(parts[0], parts[1]));
+                        System.out.println("Now you have " + (tasks.size() + 1) + " tasks in the list.");
                         System.out.println("____________________________________________________________");
                         break;
 
@@ -87,10 +87,28 @@ class Bob {
                         if (eventParts.length < 2) throw new InputExceptions.MissingArgumentException("event");
                         String[] timeParts = eventParts[1].split(" /to ", 2);
                         if (timeParts.length < 2) throw new InputExceptions.MissingArgumentException("event timing");
-                        tasks[taskCount++] = new Event(eventParts[0], timeParts[0], timeParts[1]);
-                        System.out.println("Now you have " + taskCount + " tasks in the list.");
+                        tasks.add( new Event(eventParts[0], timeParts[0], timeParts[1]));
+                        System.out.println("Now you have " + (tasks.size() + 1) + " tasks in the list.");
                         System.out.println("____________________________________________________________");
                         break;
+
+                    case "delete":
+                        if (arguments.isEmpty()) {
+                            throw new InputExceptions.MissingArgumentException("todo");
+                        }
+                        int deleteIndex = Integer.parseInt(arguments) - 1;
+                        if (deleteIndex < 0 || deleteIndex > tasks.size()) {
+                            throw new InputExceptions.InvalidIndexException();
+                        }
+                        Task removedTask = tasks.remove(deleteIndex);
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Noted. I've removed this task:");
+                        System.out.println("   " + removedTask);
+                        System.out.println("Now you have " + tasks.size() + 1 + " tasks in the list.");
+                        System.out.println("____________________________________________________________");
+                        break;
+
+
 
                     default:
                         throw new InputExceptions.InvalidCommandException();
