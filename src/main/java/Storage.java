f7e39c35babcf547ea9a4c1a5ced5c12f1c1806e
Jonathan2745 <e1157198@u.nus.edu>
2025-03-05 20:48:48 +0800

    added classes - not implemented yet

---
+++ src/main/java/Storage.java
@@ -0,0 +1,19 @@
+public class Storage {
+    private static String filePath = "data/userTasks.txt";
+
+
+    public void saveData(String data) {
+        // Implement file writing logic here
+
+    }
+
+    public String loadData() {
+        // Implement file reading logic here
+
+        return ""; // Return loaded data
+    }
+
+}
+
+
+
